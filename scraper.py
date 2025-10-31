5ce4afed22a89b08ff9db2f493b4e3d289927f00
ghadiabk <ghadiabk05@gmail.com>
2025-10-31 18:52:26 +0200

    more fixes

--- scraper.py
+++ scraper.py
@@ -2,11 +2,9 @@ import csv
 import time
 from datetime import datetime
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import os
 
