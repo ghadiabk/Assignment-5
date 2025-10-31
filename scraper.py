07ff2cc5085fa6302cc768bdd91a7596883c5eae
ghadiabk <ghadiabk05@gmail.com>
2025-10-31 20:14:34 +0200

    ...

--- scraper.py
+++ scraper.py
@@ -14,9 +14,12 @@ output_file = "ebay_tech_deals.csv"
 
 def get_driver():
     opts = Options()
-    opts.add_argument("--headless")
+    opts.add_argument("--headless=new")
     opts.add_argument("--no-sandbox")
     opts.add_argument("--disable-dev-shm-usage")
+    opts.add_argument("--disable-gpu")
+    opts.add_argument("--window-size=1920,1080")
+    opts.binary_location = "/usr/bin/chromium-browser"
     return webdriver.Chrome(options=opts)
 
 
